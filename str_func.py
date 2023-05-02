00a2ac6eed29059036f188b74f52b58463bbeebe
grbcas <grbcas@gmail.com>
2023-05-03 02:16:09 +0300

    add capitalize_str

--- str_func.py
+++ str_func.py
@@ -5,5 +5,15 @@ def upper(in_str: str) -> str:
 	return in_str.upper()
 
 
+def capitalize_str(in_str: str) -> str:
+	"""
+
+	capitalize_str(in_str: str) -> str.capitalize()
+
+	"""
+	return in_str.capitalize()
+
+
 if __name__ == '__main__':
 	print(upper('text'))
+	print(capitalize_str('test'))
